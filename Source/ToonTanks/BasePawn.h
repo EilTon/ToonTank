ddda2f936e6d8dbf7e81c231db9c1557c2da6ec6
EilTon <dylan.delaunois@hotmail.com>
2022-10-03 11:58:17 +0200

    Mergin

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -17,41 +17,37 @@ public:
 
 	void HandleDestruction();
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent *capsuleComponent;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
-		UStaticMeshComponent* turretMesh;
+	UStaticMeshComponent *turretMesh;
 
 	void RotateTurret(FVector LookAtTarget);
 
 	void Fire();
 
-
 private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
-		class UCapsuleComponent* capsuleComponent;
+	UStaticMeshComponent *baseMesh;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
-		UStaticMeshComponent* baseMesh;
+	USceneComponent *projectileSpawnPoint;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
-		USceneComponent* projectileSpawnPoint;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Base Pawn", meta = (AllowPrivateAccess = "true"))
-		TSubclassOf<class AProjectile> projectileClass;
+	TSubclassOf<class AProjectile> projectileClass;
 
 	UPROPERTY(EditAnywhere)
-		class UParticleSystem* deathParticle;
+	class UParticleSystem *deathParticle;
 
 	UPROPERTY(EditAnywhere)
-		class USoundBase* deathSound;
-
+	class USoundBase *deathSound;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-
 };
